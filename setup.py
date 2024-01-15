bbab59da55ab9070e1d33a26a260cdbbf5b360ba
Fernanda Macías <fernanda.macias@ug.uchile.cl>
2024-01-15 10:58:13 -0300

    docs: sets the readme as the library description

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="psifospoll",
     version="1.0.0",
@@ -14,4 +20,6 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
